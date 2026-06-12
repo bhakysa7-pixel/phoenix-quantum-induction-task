09b15ae8b6e53a3d72350d36c54d45ed633819f7
bhakysa7-pixel <bhakysa7@gmail.com>
2026-06-12 08:16:37 +0530

    Update bell_state.py

--- bell_state.py
+++ bell_state.py
@@ -3,7 +3,7 @@ import numpy as np
 A = np.array([1,0])
 state=np.kron(A,A)           #create a |00> state by taking the tensor product of two zero qubits
 print("zero qubit state 1 :", A)
-print("tensor product :", state )
+print("Initial 2-qubit state |00> :", state)
 
 H=1/np.sqrt(2) * np.array([[1,1],[1,-1]])
 print("HADAMARD GATE :", H) 
@@ -13,7 +13,7 @@ I = np.array([
     [0,1]
 ])
 
-H_first = np.kron(H,I) 
+H_first = np.kron(H,I)  # Apply Hadamard to the first qubit and Identity to the second qubit
 
 result = H_first @ state #apply the hadamard gate on the first qubit of the |00> state
 print("after applying hadamard gate on first qubit ")
@@ -25,9 +25,10 @@ CNOT = np.array([
     [0,0,0,1],
     [0,0,1,0]
 ])
-bell_state = CNOT @ result  #apply the CNOT gate to create the Bell state
-print("Bell state :")
-print(bell_state)
+bell_state = CNOT @ result   # Apply CNOT: 
+print("Bell state :")        # |00> remains |00>  # |10> becomes |11>  
+print(bell_state)            # Result: (|00> + |11>)/√2
+
 
 p=np.abs(bell_state)**2  #compute the probabilities 
 print("00 probability :", p[0])
@@ -37,7 +38,7 @@ print("11 probability :", p[3])
 outcomes = ['00','01','10','11']
 answer = np.random.choice(outcomes,p=p) #simulate measurement by randomly choosing an outcome based on the probabilities 
 print("outcomes are : ", outcomes)
-print(answer)
+print("Measured State :", answer)
 
 
 
